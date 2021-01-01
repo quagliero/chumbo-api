0aa02b75ac6ad7921dfaa2ac5fb74de4f7cbfeed
Commish <>
2021-01-01 21:33:08 +0000

    Redirect / to the api docs

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,5 @@
 module.exports = (app) => {
-  /**
-  * @swagger
-  *
-  * /:
-  *   get:
-  *     summary: "Get default route"
-  *     produces:
-  *       - "application/json"
-  *     responses:
-  *       "200":
-  *         description: "Returns welcome message."
-  *         content:
-  *           application/json
-  */
   app.get('/', async (req, res, next) => {
-    res.status(200).json({
-      message: 'Welcome to your default route...'
-    })
+    res.redirect(301, '/api-docs');
   });
 }
